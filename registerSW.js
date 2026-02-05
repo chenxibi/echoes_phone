bc7269ecc93c3b6eabad1c7a0fade29302de1b52
chenxibi <bcx0216@icloud.com>
2026-02-05 12:29:32 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/echoes-phone/sw.js', { scope: '/echoes-phone/' })})}
