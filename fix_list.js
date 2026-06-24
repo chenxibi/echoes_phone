e6cdcd74d9251e9e14fcf114722ee248adf774bf
bichenxi <bichenxi@users.noreply.github.com>
2026-06-24 09:18:50 +0100

    deploy: 2026-06-24 16:18 - simplify OP identity

--- fix_list.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('C:/Users/bichenxi/echoes_phone/src/utils/appHelpers.jsx', 'utf8');
-const old = '\r\n  { id: "forum", label: "生活圈", icon: Hash },\r\n';
-const nw = '\r\n  { id: "feedback", label: "反馈", icon: MessageSquare },\r\n  { id: "forum", label: "生活圈", icon: Hash },\r\n';
-const idx = c.indexOf(old);
-console.log('idx:', idx);
-c = c.substring(0, idx) + nw + c.substring(idx + old.length);
-fs.writeFileSync('C:/Users/bichenxi/echoes_phone/src/utils/appHelpers.jsx', c, 'utf8');
-console.log('OK, contains feedback:', c.includes('feedback'));
